c23cb7abcf04fb9798f31cbd78126cf88a61b041
Tymovskyi <ded.lviv@gmail.com>
2020-03-04 21:12:45 +0200

    Updates

---
+++ precache-manifest.498a49ea7c2fa698a0a6668d12b55525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a492cdfe1557cf676a1d1f01c9ac3f8c",
+    "url": "/user-app/index.html"
+  },
+  {
+    "revision": "7a7ccd516527c483f789",
+    "url": "/user-app/static/css/main.3cfa3716.chunk.css"
+  },
+  {
+    "revision": "849c41bca0203e53b85d",
+    "url": "/user-app/static/js/2.e6884300.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/user-app/static/js/2.e6884300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7ccd516527c483f789",
+    "url": "/user-app/static/js/main.4f0fa752.chunk.js"
+  },
+  {
+    "revision": "2007ac64224bd8b78a27",
+    "url": "/user-app/static/js/runtime-main.c83cc26b.js"
+  }
+]);
